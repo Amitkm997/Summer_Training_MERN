a6625e4bb5fadae056e8c4cfa9e6ec78e1c240f1
Amitkm997 <amitkm997@gmail.com>
2026-06-28 11:34:41 +0530

    company_controller_completed

--- Backend/routes/companyRoute.js
+++ Backend/routes/companyRoute.js
@@ -1,8 +1,12 @@
 
 import express from 'express';
-import {addCompany} from '../controllers/companyController.js'
+import {addCompany,getCompany,getCompanyById, updateCompany,deleteCompany} from '../controllers/companyController.js'
 const router = express.Router();
 
 router.post('/',addCompany)
+router.get('/',getCompany)
+router.get('/:id',getCompanyById)
+router.put('/:id',updateCompany)
+router.delete('/:id',deleteCompany)
 
 export default router;
